16fc517362371534803c1d6c7a2eaa44111ef322
minh <mreigen@gmail.com>
2014-02-09 17:03:04 -0800

    init commit - rails 4 and mongodb - commented out active record

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gratitude::Application.config.secret_key_base = '741f9c46cb305599bb6ec535193fc697c54e8cc72756cfef9edb30a4ff311f3a20bff433a9efa57ca6a99e94ef47a9254d630389e3121e1fa0c16bdf10f83827'
